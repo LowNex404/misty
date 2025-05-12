ed52146c99e9fa0e715bdf88ceb6ca6cb16da9b5
Fred Pereira <159783368+LowNex404@users.noreply.github.com>
2025-05-12 16:58:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -118,4 +118,4 @@ app.get('/api/user', ensureAuthenticated, async (req, res) => {
 
 // Listen
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando em https://misty-bot.onrender.com`));
